0eb125310aec43baf07f7bb63eaccdea1b757b50
kabeer11000 <52799968+kabeer11000@users.noreply.github.com>
2020-09-28 15:54:25 +0500

    Code Save Commit

--- client/src/components/SearchComponentTV/SearchComponentTV.js
+++ client/src/components/SearchComponentTV/SearchComponentTV.js
@@ -22,6 +22,8 @@ import Container from "@material-ui/core/Container";
 import SongCard from "../SongCard/SongCard.lazy";
 import {FocusNode} from "@please/lrud";
 import {storageIndex} from "../../functions/Helper/storageIndex";
+import Slide from "@material-ui/core/Slide";
+import Grow from "@material-ui/core/Grow";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -63,18 +65,20 @@ const SearchComponentTV = (props) => {
 	const abortController = new AbortController();
 	const [songsResult, setSongsResult] = React.useState(undefined);
 	const [listItems, setListItems] = React.useState(
-		<div className={"errorPage text-center"}
-			 style={{
-				 position: "absolute",
-				 top: "50%",
-				 left: "22.5%",
-				 transform: "translate(-50%, -50%)"
-			 }}>
-			<img src={"./assets/icons/darkmode_nothingfound.svg"} style={{width: "8rem", height: "auto"}}
-				 alt={"Kabeers Music Logo"}/>
-			<br/>
-			<div>{navigator.onLine ? "Results will appear as you type" : "Searching In Downloads"}</div>
-		</div>);
+		<Grow in={true}>
+			<div className={"errorPage text-center"}
+				 style={{
+					 position: "absolute",
+					 top: "50%",
+					 left: "22.5%",
+					 transform: "translate(-50%, -50%)"
+				 }}>
+				<img src={"./assets/icons/darkmode_nothingfound.svg"} style={{width: "8rem", height: "auto"}}
+					 alt={"Kabeers Music Logo"}/>
+				<br/>
+				<div>{navigator.onLine ? "Results will appear as you type" : "Searching In Downloads"}</div>
+			</div>
+		</Grow>);
 	const classes = useStyles();
 	const ListItems = async () => {
 		if (!queryArray) return;
@@ -160,120 +164,127 @@ const SearchComponentTV = (props) => {
 	}
 
 	return (
-		<div style={{height: "100%"}}>
-			<div style={{width: "100%", display: "inline-flex", height: "49vh", justifyContent: "space-between"}}>
-				{
-					/*
-								<AppBar className={`fixed-top bg-transparent`} elevation={0}>
-					<Toolbar><Typography variant={"h4"}/>{store.getState().q}</Toolbar>
-				</AppBar>
-	*/
-				}
-				<FocusNode
-					onFocused={() => setOpen(prevState => ({
-						...prevState,
-						listItems: false,
-					}))}
-					onBlurred={() => setOpen(prevState => ({
-						...prevState,
-						listItems: true,
-					}))}>
-					<Paper component="form" className={`ml-5 mt-5 ${classes.root}`}>
-						<IconButton>
-							<SearchIcon/>
-						</IconButton>
-						<Divider className={classes.divider} orientation="vertical"/>
-						<InputBase
-							autoFocus
-							className={classes.input}
-							value={open.queryValue}
-							placeholder="Search Kabeers Music"
-							inputProps={{"aria-label": "search kabeers music"}}
-							onChange={(e) => Search(e.target.value)}
-						/>
-					</Paper>
-					<br/>
-					<Paper className={`ml-5`}
-						   style={{maxWidth: 400, maxHeight: "30vh", overflow: "hidden"}} /* hidden={open.listItems} */>
-						{listItems}
-					</Paper>
-				</FocusNode>
-				<div>
-					<div style={{width: "50vw", color: "black", height: "100%"}} className={"bg-transparent mr-5 mt-5"}>
-						{
-							localStorage.getItem(storageIndex.onScreenKeyboard) !== null ? (
-								<Keyboard
-									className={"bg-transparent h-100 w-100"}
-									onChange={Search}
-									display={{
-										"{bksp}": "<svg class=\"MuiSvgIcon-root jss172\" focusable=\"false\" viewBox=\"0 0 24 24\" aria-hidden=\"true\"><path d=\"M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 12.59L17.59 17 14 13.41 10.41 17 9 15.59 12.59 12 9 8.41 10.41 7 14 10.59 17.59 7 19 8.41 15.41 12 19 15.59z\"></path></svg>",
-										"{enter}": "< enter",
-										"{shift}": "shift",
-										"{s}": "shift",
-										"{tab}": "tab",
-										"{lock}": "caps",
-										"{accept}": "Submit",
-										"{space}": `<svg class="MuiSvgIcon-root jss172" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M18 9v4H6V9H4v6h16V9z"></path></svg>`,
-										"{//}": " "
-									}}
-									{...localStorage.getItem(storageIndex.onScreenKeyboard) ? ({
-										theme: "hg-theme-default"
-									}) : null}
-									layout={{
-										"default": [
-											"1 2 3 4 5 6 7 8 9 0",
-											"q w e r t y u i o p",
-											"a s d f g h j k l .com",
-											"z x c v b n m @ {bksp}",
-											"{space}"
-										],
-										"shift": [
-											"~ ! @ # $ % ^ & * ( ) _ + {bksp}",
-											"{tab} Q W E R T Y U I O P { } |",
-											"{lock} A S D F G H J K L : \" {enter}",
-											"{shift} Z X C V B N M < > ? {shift}",
-											".com @ {space}"
-										]
-									}}
-									onKeyPress={(e) => {
-										console.log(e);
-									}}
-								/>
-							) : null
-						}
-					</div>
-				</div>
-			</div>
-			{/*hidden={!open.listItems}*/}
-			<div className={"mt-0"}>
-				{songsResult && (navigator.onLine ? (songsResult.items && songsResult.items.length) : true) ? (
-					<Container maxWidth="xl" className={"px-0 mx-0"} style={{maxWidth: "100vw"}}>
-						<div className={"cardSlider Slider"}>
+		<Slide in={true} direction={"left"}>
+			<div style={{height: "100%"}}>
+				<div style={{width: "100%", display: "inline-flex", height: "49vh", justifyContent: "space-between"}}>
+					{
+						/*
+									<AppBar className={`fixed-top bg-transparent`} elevation={0}>
+						<Toolbar><Typography variant={"h4"}/>{store.getState().q}</Toolbar>
+					</AppBar>
+		*/
+					}
+					<FocusNode
+						onFocused={() => setOpen(prevState => ({
+							...prevState,
+							listItems: false,
+						}))}
+						onBlurred={() => setOpen(prevState => ({
+							...prevState,
+							listItems: true,
+						}))}>
+						<Paper component="form" className={`ml-5 mt-5 ${classes.root}`}>
+							<IconButton>
+								<SearchIcon/>
+							</IconButton>
+							<Divider className={classes.divider} orientation="vertical"/>
+							<InputBase
+								autoFocus
+								className={classes.input}
+								value={open.queryValue}
+								placeholder="Search Kabeers Music"
+								inputProps={{"aria-label": "search kabeers music"}}
+								onChange={(e) => Search(e.target.value)}
+							/>
+						</Paper>
+						<br/>
+						<Paper className={`ml-5`}
+							   style={{
+								   maxWidth: 400,
+								   maxHeight: "30vh",
+								   overflow: "hidden"
+							   }} /* hidden={open.listItems} */>
+							{listItems}
+						</Paper>
+					</FocusNode>
+					<div>
+						<div style={{width: "50vw", color: "black", height: "100%"}}
+							 className={"bg-transparent mr-5 mt-5"}>
 							{
-								navigator.onLine ?
-									(songsResult.items.map((video, index) => (
-										<SongCard key={index}
-												  thumbnail={video.snippet.thumbnails.high.url}
-												  key_={index}
-												  video={video}
-												  onPlay={() => PlaySong(video, {
-													  list: songsResult, index: index
-												  })}
-												  list={songsResult}/>
-									))) : (songsResult.map((video, index) => (
-										<SongCard key={index}
-												  thumbnail={URL.createObjectURL(video.item.thumbnail)}
-												  key_={index}
-												  video={video.item.videoElement}
-												  onPlay={() => PlayOfflineSong(video.item, index)}
-												  list={[]}/>
-									)))
+								localStorage.getItem(storageIndex.onScreenKeyboard) !== null ? (
+									<Keyboard
+										className={"bg-transparent h-100 w-100"}
+										onChange={Search}
+										display={{
+											"{bksp}": "<svg class=\"MuiSvgIcon-root jss172\" focusable=\"false\" viewBox=\"0 0 24 24\" aria-hidden=\"true\"><path d=\"M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 12.59L17.59 17 14 13.41 10.41 17 9 15.59 12.59 12 9 8.41 10.41 7 14 10.59 17.59 7 19 8.41 15.41 12 19 15.59z\"></path></svg>",
+											"{enter}": "< enter",
+											"{shift}": "shift",
+											"{s}": "shift",
+											"{tab}": "tab",
+											"{lock}": "caps",
+											"{accept}": "Submit",
+											"{space}": `<svg class="MuiSvgIcon-root jss172" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M18 9v4H6V9H4v6h16V9z"></path></svg>`,
+											"{//}": " "
+										}}
+										{...localStorage.getItem(storageIndex.onScreenKeyboard) ? ({
+											theme: "hg-theme-default"
+										}) : null}
+										layout={{
+											"default": [
+												"1 2 3 4 5 6 7 8 9 0",
+												"q w e r t y u i o p",
+												"a s d f g h j k l .com",
+												"z x c v b n m @ {bksp}",
+												"{space}"
+											],
+											"shift": [
+												"~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+												"{tab} Q W E R T Y U I O P { } |",
+												"{lock} A S D F G H J K L : \" {enter}",
+												"{shift} Z X C V B N M < > ? {shift}",
+												".com @ {space}"
+											]
+										}}
+										onKeyPress={(e) => {
+											console.log(e);
+										}}
+									/>
+								) : null
 							}
 						</div>
-					</Container>) : null
-				}
+					</div>
+				</div>
+				{/*hidden={!open.listItems}*/}
+				<div className={"mt-0"}>
+					{songsResult && (navigator.onLine ? (songsResult.items && songsResult.items.length) : true) ? (
+						<Container maxWidth="xl" className={"px-0 mx-0"} style={{maxWidth: "100vw"}}>
+							<div className={"cardSlider Slider"}>
+								{
+									navigator.onLine ?
+										(songsResult.items.map((video, index) => (
+											<SongCard key={index}
+													  thumbnail={video.snippet.thumbnails.high.url}
+													  key_={index}
+													  video={video}
+													  onPlay={() => PlaySong(video, {
+														  list: songsResult, index: index
+													  })}
+													  list={songsResult}/>
+										))) : (songsResult.map((video, index) => (
+											<SongCard key={index}
+													  thumbnail={URL.createObjectURL(video.item.thumbnail)}
+													  key_={index}
+													  video={video.item.videoElement}
+													  onPlay={() => PlayOfflineSong(video.item, index)}
+													  list={[]}/>
+										)))
+								}
+							</div>
+						</Container>) : null
+					}
+				</div>
 			</div>
-		</div>
+		</Slide>
 	);
 };
 
